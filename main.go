31e4f844f5645575e4d60cc1e33f297189c1f8d6
ranidentity <chiewwei1991@gmail.com>
2025-07-27 00:52:19 +0800

    cleanup

--- main.go
+++ main.go
@@ -13,14 +13,12 @@ import (
 // @host localhost:8080
 // @BasePath /
 func main() {
-	// Setup db connection
 	db, err := database.ConnectDB()
 	if err != nil {
 		log.Fatalf("Failed to connect to db :%v", err)
 	}
 	defer database.CloseDB(db)
 	validate := validator.New()
-	// Register the custom validation function for "decimal2" tag here in main.go
 
 	container := router.Build(db, validate)
 	router := router.RegisterRoutes(container)
--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
 1. docker-compose up -d
 2. go mod tidy
 3. make a copy of .env from example.env
+
+
+System flow
+- main -> router -> controller -> service -> repo -> model -> database
+
+API documentation - Swagger
+http://localhost:8080/swagger/index.html
+
+Sample postman collection is included
